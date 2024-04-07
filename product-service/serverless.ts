4769c50e41aa202eead1d9f0d8e998d4a5344805
Yurii Yarovyi <yurii.yarovyi@dat.com>
2024-04-07 09:31:32 +0300

    add policies for DB access, refactor service to retrieve from DB

--- product-service/serverless.ts
+++ product-service/serverless.ts
@@ -6,6 +6,7 @@ const serverlessConfiguration: AWS = {
   service: "product-service",
   frameworkVersion: "3",
   plugins: [
+    "serverless-dynamodb-local",
     "serverless-auto-swagger",
     "serverless-esbuild",
     "serverless-offline",
@@ -23,6 +24,8 @@ const serverlessConfiguration: AWS = {
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: "1",
       NODE_OPTIONS: "--enable-source-maps --stack-trace-limit=1000",
+      TABLE_NAME_PRODUCT: "${self:custom.productTableName}",
+      TABLE_NAME_STOCK: "${self:custom.stockTableName}",
     },
     httpApi: {
       cors: true,
@@ -33,6 +36,7 @@ const serverlessConfiguration: AWS = {
     getProductsList: {
       ...getProductsList,
       name: "${sls:stage}-getProductsList",
+      role: "GetProductsLambdaRole",
       events: [
         {
           httpApi: {
@@ -45,6 +49,7 @@ const serverlessConfiguration: AWS = {
     getProductsById: {
       ...getProductsById,
       name: "${sls:stage}-getProductsById",
+      role: "GetProductByIdLambdaRole",
       events: [
         {
           httpApi: {
@@ -66,6 +71,151 @@ const serverlessConfiguration: AWS = {
       define: { "require.resolve": undefined },
       platform: "node",
       concurrency: 10,
+    },
+    productTableName: "Product",
+    stockTableName: "Stock",
+    dynamodb: {
+      stages: ["dev"],
+      start: {
+        docker: true,
+        port: 8000,
+        inMemory: true,
+        migrate: true,
+        seed: true,
+        convertEmptyValues: true,
+      },
+      seed: {
+        domain: {
+          sources: [
+            {
+              table: "self:custom.productTableName",
+              sources: ["./seed/Products.json"],
+            },
+            {
+              table: "self:custom.stockTableName",
+              sources: ["./seed/Stocks.json"],
+            },
+          ],
+        },
+      },
+    },
+  },
+  resources: {
+    Resources: {
+      // IAM role for Lambda functions
+      GetProductsLambdaRole: {
+        Type: "AWS::IAM::Role",
+        Properties: {
+          RoleName: "GetProductsLambda-${sls:stage}",
+          AssumeRolePolicyDocument: {
+            Version: "2012-10-17",
+            Statement: [
+              {
+                Effect: "Allow",
+                Principal: {
+                  Service: "lambda.amazonaws.com",
+                },
+                Action: "sts:AssumeRole",
+              },
+            ],
+          },
+          Policies: [
+            {
+              PolicyName: "DynamoDBGetProductsPolicy",
+              PolicyDocument: {
+                Version: "2012-10-17",
+                Statement: [
+                  {
+                    Sid: "DynamoDBGetProductsPolicy",
+                    Effect: "Allow",
+                    Action: ["dynamodb:Scan", "dynamodb:GetItem"],
+                    Resource: [
+                      "arn:aws:dynamodb:${self:provider.region}:*:table/${self:custom.productTableName}",
+                      "arn:aws:dynamodb:${self:provider.region}:*:table/${self:custom.stockTableName}",
+                    ],
+                  },
+                ],
+              },
+            },
+          ],
+        },
+      },
+      GetProductByIdLambdaRole: {
+        Type: "AWS::IAM::Role",
+        Properties: {
+          RoleName: "GetProductByIdLambda-${sls:stage}",
+          AssumeRolePolicyDocument: {
+            Version: "2012-10-17",
+            Statement: [
+              {
+                Effect: "Allow",
+                Principal: {
+                  Service: "lambda.amazonaws.com",
+                },
+                Action: "sts:AssumeRole",
+              },
+            ],
+          },
+          Policies: [
+            {
+              PolicyName: "DynamoDBGetProductsPolicy",
+              PolicyDocument: {
+                Version: "2012-10-17",
+                Statement: [
+                  {
+                    Sid: "DynamoDBGetProductsPolicy",
+                    Effect: "Allow",
+                    Action: ["dynamodb:Scan", "dynamodb:GetItem"],
+                    Resource: [
+                      "arn:aws:dynamodb:${self:provider.region}:*:table/${self:custom.productTableName}",
+                      "arn:aws:dynamodb:${self:provider.region}:*:table/${self:custom.stockTableName}",
+                    ],
+                  },
+                ],
+              },
+            },
+          ],
+        },
+      },
+      // DynamoDB tables
+      ProductTable: {
+        Type: "AWS::DynamoDB::Table",
+        Properties: {
+          TableName: "Product",
+          AttributeDefinitions: [
+            {
+              AttributeName: "id",
+              AttributeType: "S",
+            },
+          ],
+          KeySchema: [
+            {
+              AttributeName: "id",
+              KeyType: "HASH",
+            },
+          ],
+          BillingMode: "PAY_PER_REQUEST",
+        },
+      },
+      StockTable: {
+        Type: "AWS::DynamoDB::Table",
+        Properties: {
+          TableName: "Stock",
+          AttributeDefinitions: [
+            {
+              AttributeName: "product_id",
+              AttributeType: "S",
+            },
+          ],
+          KeySchema: [
+            {
+              AttributeName: "product_id",
+              KeyType: "HASH",
+            },
+          ],
+          BillingMode: "PAY_PER_REQUEST",
+        },
+      },
     },
   },
 };
--- product-service/src/services/product-service.ts
+++ product-service/src/services/product-service.ts
@@ -1,9 +1,60 @@
 import { products } from "@functions/getProductsList/mocks";
+import {
+  DynamoDBClient,
+  GetItemCommand,
+  ScanCommand,
+} from "@aws-sdk/client-dynamodb";
+import { unmarshall } from "@aws-sdk/util-dynamodb";
+
+// CONSTANTS
+const TABLE_NAME_PRODUCT = process.env.TABLE_NAME_PRODUCT;
+const TABLE_NAME_STOCK = process.env.TABLE_NAME_STOCK;
+const dynamoDb = new DynamoDBClient();
+
+const retrieveStockAndMerge = async (product) => {
+  const command = new GetItemCommand({
+    TableName: TABLE_NAME_STOCK,
+    Key: {
+      product_id: { S: product.id },
+    },
+  });
+
+  const { Item } = await dynamoDb.send(command);
+  if (Item) {
+    const stock = unmarshall(Item);
+    return { ...product, ...stock };
+  }
+
+  return product;
+};
+
+const retrieveStocksAndMerge = async (products) => {
+  return Promise.all(products.map(retrieveStockAndMerge));
+};
 
 export const getProductsList = async () => {
-  return await products;
+  const command = new ScanCommand({
+    TableName: TABLE_NAME_PRODUCT,
+  });
+
+  const { Items } = await dynamoDb.send(command);
+  const products = Items.map((item) => unmarshall(item));
+  const res = await retrieveStocksAndMerge(products);
+  return res;
 };
 
 export const getProductById = async (id) => {
-  return products.find((p) => p.id === id) || null;
+  const command = new GetItemCommand({
+    TableName: TABLE_NAME_PRODUCT,
+    Key: {
+      id: { S: id },
+    },
+  });
+
+  const { Item } = await dynamoDb.send(command);
+  if (!Item) {
+    return null;
+  }
+
+  return retrieveStockAndMerge(unmarshall(Item));
 };
